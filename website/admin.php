059652e58702b99434c655f60e405d50df0f4219
Mark Hebberd <markhebberd@gmail.com>
2026-05-10 04:27:14 +1200

    5 minute time limit for sightings import

--- website/admin.php
+++ website/admin.php
@@ -398,6 +398,7 @@ if ($action === 'import_penguins' || $action === 'trial_reimport_penguins') {
 }
 
 if ($action === 'import_sightings' || $action === 'trial_import_sightings') {
+    set_time_limit(300);
     $dryRun = ($action === 'trial_import_sightings');
     $monitorPrefix = 'sheet-import';
     $colonyId = 1; $observerId = 1;
